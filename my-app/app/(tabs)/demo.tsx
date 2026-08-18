59e1709bc46809c3dd04382e128278b25ebce51f
= <=>
2026-08-18 13:42:33 +0300

    first commit

---
+++ my-app/app/(tabs)/demo.tsx
@@ -0,0 +1,20 @@
+import { Text, View } from "react-native";
+
+export default function DemoScreen() {
+    return (
+        <View style={
+            {
+                backgroundColor: "pink",
+                borderWidth: 5,
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                gap: 15
+            }
+        }>
+            <Text>
+                Hello youuuuu !
+            </Text>
+        </View>
+    )
+}
