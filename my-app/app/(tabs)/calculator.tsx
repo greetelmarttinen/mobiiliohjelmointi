e3fa06798083a4fa07a7fb74e354d41be50d08cd
= <=>
2026-08-24 11:57:21 +0300

    Add text input fields (only styles, not logic)

--- my-app/app/(tabs)/calculator.tsx
+++ my-app/app/(tabs)/calculator.tsx
@@ -1,6 +1,11 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+
 
 export default function Calculator() {
+
+    // luodaan state ja asetetaan sen alkuarvo tyhjäksi
+    const [text, setText] = useState("");
 
     return (
         <View style={
@@ -12,14 +17,81 @@ export default function Calculator() {
                 alignItems: "center"
             }
         }>
-            <Text style={
+            <View style={
                 {
-                    fontSize: 25,
-                    fontWeight: "bold"
+                    flexDirection: "column",
+                    alignItems: "center"
                 }
             }>
-                Calculator
-            </Text>
-        </View>
+                <View>
+                    <Text style={
+                        {
+                            fontSize: 25,
+                            fontWeight: "bold"
+                        }
+                    }>
+                        Calculator
+                    </Text>
+                </View>
+
+
+                <View>
+                    <Text style={
+                        {
+                            fontSize: 18,
+                            color: "black"
+                        }
+                    }>
+                        Result:
+                    </Text>
+                </View>
+
+
+
+                <View>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Enter a number"
+                        placeholderTextColor={"black"}
+                        onChangeText={text => setText(text)}
+                        value={text}
+                    />
+                </View>
+
+                <View>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Enter a number"
+                        placeholderTextColor={"black"}
+                        onChangeText={text => setText(text)}
+                        value={text}
+                    />
+                </View>
+
+
+
+
+
+
+
+            </View>
+
+
+
+
+
+        </View >
+
+
     )
 }
+
+const styles = StyleSheet.create({
+    // input -elementtien tyylitys
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10
+    }
+})
