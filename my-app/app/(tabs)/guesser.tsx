44a14dac203f1a1ee803fec3ef0554aa6bf22dce
= <=>
2026-08-30 14:38:42 +0300

    Add Guesser game layout

--- my-app/app/(tabs)/guesser.tsx
+++ my-app/app/(tabs)/guesser.tsx
@@ -1,3 +1,4 @@
+import { Keyboard, StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
 
 
 export default function Guesser() {
@@ -9,7 +10,32 @@ export default function Guesser() {
 
 
     return (
-        
+        // sallitaan avoinna olevan näppäimistön sulkeminen, kun käyttäjä painaa alueesta
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={styles.container}>
+                <View>
+                    <Text style={styles.headerText}>
+                        Guesser game
+                    </Text>
+                </View>
+            </View>
+
+        </TouchableWithoutFeedback>
     )
 
 }
+
+// tyylit
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#D8B4FE",
+        borderWidth: 5,
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    headerText: {
+        fontSize: 25,
+        fontWeight: "bold"
+    }
+})
