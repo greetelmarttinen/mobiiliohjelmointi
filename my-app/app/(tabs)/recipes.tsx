5470d2c315198950c13ed31c4467c5baedc1c7b2
= <=>
2026-09-25 14:15:15 +0300

    Create main components to the page (header text, text input, find -button)

--- my-app/app/(tabs)/recipes.tsx
+++ my-app/app/(tabs)/recipes.tsx
@@ -1,3 +1,4 @@
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 
 // tyypitetään repository -objekti
 type Repository = {
@@ -5,6 +6,85 @@ type Repository = {
     strMeal: string;
     strMealThumb: string;
 };
+
+export default function Recipes() {
+
+    const handleFetch = () => {
+
+    }
+
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.components}>
+                <Text style={styles.headerText}>
+                    Recipe search
+                </Text>
+                <View>
+                    {/** hakukenttä */}
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Enter an ingredient..."
+                        placeholderTextColor={"black"}
+                        textAlign="center"
+                    //value={keyword}
+                    //onChangeText={text => setKeyword(text)}
+                    />
+                </View>
+
+                <View>
+                    <Pressable onPress={handleFetch}>
+                        <Text style={styles.button}>
+                            Find
+                        </Text>
+                    </Pressable>
+                </View>
+
+            </View>
+
+
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#e6a28a",
+        borderWidth: 5,
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    components: {
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 15
+    },
+    headerText: {
+        fontSize: 25,
+        fontWeight: "bold"
+    },
+    input: {
+        height: 40,
+        width: 160,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10
+    },
+    button: {
+        backgroundColor: "black",
+        borderRadius: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        justifyContent: "center",
+        alignItems: "center",
+        color: "white",
+        fontWeight: "bold"
+    }
+})
+
+
 
 
 
